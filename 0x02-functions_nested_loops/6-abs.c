7fcb745d3e0a82d0f887df198a95d0c25962fe70
LuisPatino92 <2450@holberton.com>
2020-09-22 21:51:31 +0000

    File creation

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _abs - Compute the absolute value of the input
+ *
+ * @n: Value whose absolute value is needed
+ * Return: absValue the absolute value of n
+ */
+
+int _abs(int n);
+{
+	int absValue;
+
+	if (n < 0)
+		absValue = -1 * n;
+	else
+		absValue = n;
+
+	return (absValue);
+
+}
