f86fb7d7a554007eced0c58a0d6419e98836b5e8
LuisPatino92 <2450@holberton.com>
2020-09-23 03:04:03 +0000

    With 4 loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 	for (i = 0; i <= 2; i++)
 	{
 
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 9; j++)
 		{
 
 			for (k = 0; k <= 5; k++)
@@ -27,6 +27,9 @@ void jack_bauer(void)
 					_putchar(k + '0');
 					_putchar(l + '0');
 					_putchar('\n');
+
+					if (i == 2 && j == 3 && k == 5 && l == 9)
+						return;
 
 				}
 
