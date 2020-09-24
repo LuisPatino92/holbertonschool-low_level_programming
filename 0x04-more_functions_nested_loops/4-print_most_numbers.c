b8a9bf2e0f5aba8ab16de2ca5f35249df12ef18a
LuisPatino92 <2450@holberton.com>
2020-09-24 20:34:42 +0000

    File Creation

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print numbers from 1 to 9 omitting 2 and 4
+ *
+ * Return: Nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar(10);
+}
