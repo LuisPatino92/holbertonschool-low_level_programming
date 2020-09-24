d9996e9741ea0514b0a6e8b4d08e5ae7a137927f
LuisPatino92 <2450@holberton.com>
2020-09-24 20:35:05 +0000

    File Creation

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Print numbers from 0 to 14 (10 times)
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar('0' + (i / 10));
+			}
+
+			_putchar('0' + (i % 10));
+
+		}
+
+	_putchar(10);
+	}
+}
