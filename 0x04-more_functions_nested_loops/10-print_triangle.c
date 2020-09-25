5477effb08556d8cb486ae460b205118d93fec4f
LuisPatino92 <2450@holberton.com>
2020-09-25 04:25:06 +0000

    Requirement correction

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,6 +3,8 @@
 /**
  * print_triangle - Print a triangle with the desired size
  *
+ * @size: The size of desired triangle
+ *
  * Return: Nothing
  */
 
@@ -10,14 +12,21 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j <= (size - i); j++)
-			_putchar(32);
-
-		for (j = 0; j < i; j++)
-			_putchar(35);
-
 		_putchar(10);
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j <= (size - i); j++)
+				_putchar(32);
+
+			for (j = 0; j < i; j++)
+				_putchar(35);
+
+			_putchar(10);
+		}
 	}
 }
