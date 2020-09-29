b5aa66bb710cf9dc7e931ffb4fac45fefc20717c
LuisPatino92 <2450@holberton.com>
2020-09-29 12:19:48 +0000

    File Creation

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Changes the integer allocated
+ *
+ * @n: The addres passed from the main function.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
