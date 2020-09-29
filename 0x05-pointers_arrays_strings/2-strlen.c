ae14e0fd351e6bfecdc310e660b1c2a1615b27f7
LuisPatino92 <2450@holberton.com>
2020-09-29 12:20:25 +0000

    File Creation

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Returns the lenght os a string
+ *
+ * @s: The input string
+ *
+ * Return: The lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+		i++;
+
+	return (i);
+}
