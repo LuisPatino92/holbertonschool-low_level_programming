568ec03270ba64bf38d057e09d0435ecfdabdca9
LuisPatino92 <2450@holberton.com>
2020-09-29 23:15:09 +0000

    File creation

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints the last half of a string
+ *
+ * @str: The string to be half printed
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, lenght, start;
+
+	while (str[i] != 0)
+		i++;
+
+	lenght = i;
+
+	if (lenght % 2 == 0)
+		start = lenght / 2;
+	else
+		start = (lenght - 1) / 2;
+
+	for (i = start; i < lenght; i++)
+		_putchar(str[i]);
+
+	_putchar(10);
+}
