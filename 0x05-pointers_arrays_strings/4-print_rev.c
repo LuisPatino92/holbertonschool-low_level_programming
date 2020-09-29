ad48d0c73d30eec763872d89c1048be49dde54cb
LuisPatino92 <2450@holberton.com>
2020-09-29 12:21:11 +0000

    File Creation

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ *
+ * @s: The string to be printed in reverse
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, lenght;
+
+	i = 0;
+
+	while (s[i] != 0)
+		i++;
+
+	lenght = i;
+
+	for (i = 0; i <= lenght; i++)
+		_putchar(s[lenght - i]);
+
+	_putchar(10);
+}
