03cb0695fb8d28e0904d67e80fc37ae4d10285ba
LuisPatino92 <2450@holberton.com>
2020-09-29 22:16:04 +0000

    With a big array

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse a String in its address memory
+ *
+ * @s: The string to be reversed
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i, lenght;
+	char auxString[100];
+
+	i = 0;
+
+	while (s[i] != 0)
+	{
+		auxString[i] = s[i];
+		i++;
+	}
+
+	lenght = i - 1;
+
+	for (i = 0; i <= lenght; i++)
+		*(s + i) = auxString[lenght - i];
+
+}
