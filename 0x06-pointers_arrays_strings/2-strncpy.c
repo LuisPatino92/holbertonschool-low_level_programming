e475bccc66ba595d5bd0e75ff50629cd75707aa6
LuisPatino92 <2450@holberton.com>
2020-10-01 18:11:46 +0000

    File Creation

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strncpy - copy n bytes of a string
+ *
+ * @dest: The addres of the copy
+ * @src: The address of the original
+ * @n: number of bytes to be copied
+ *
+ * Return: The address of the concatenated char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, lenghtOriginal;
+
+	for (lenghtOriginal = 0; *(src + lenghtOriginal); lenghtOriginal++)
+		;
+
+	if (n > lenghtOriginal)
+	{
+		for (i = 0; i < lenghtOriginal; i++)
+		{
+			*(dest + i) = *(src + i);
+		}
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			*(dest + i) = *(src + i);
+		}
+	}
+
+	return (dest);
+}
