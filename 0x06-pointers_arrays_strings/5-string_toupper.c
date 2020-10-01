af0ed2e5d6f0288f1d2ac967c47c3d320ac53c2a
LuisPatino92 <2450@holberton.com>
2020-10-01 22:59:52 +0000

    File Creation

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * string_tuupper - Capitalizes a string
+ *
+ * @toUP: String to be capitalized
+ * Return: Capitelized string
+ */
+
+char *string_toupper(char *toUP)
+{
+
+	int i, j;
+
+	for (i = 0; *(toUP + i); i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			if (toUP[i] == j)
+				toUP[i] = j - 32;
+		}
+
+	}
+
+	return (toUP);
+}
