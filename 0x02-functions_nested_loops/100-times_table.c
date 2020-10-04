f48d897a617a011ee462514044c16b47d5975bf9
LuisPatino92 <2450@holberton.com>
2020-10-04 04:02:46 +0000

    Betty correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,51 +9,33 @@
 
 void print_times_table(int n)
 {
-	int i, j, num;
-
-	if (n < 0 || n > 15)
+int i, j, nm;
+if (n < 0 || n > 15)
+    _putchar(10);
+else
+{
+    for (i = 0; i <= n; i++)
+    {
+	for (j = 0; j <= n; j++)
 	{
-		_putchar(10);
-	}
+	nm = i * j;
+	if (nm < 10)
+	    _putchar(nm + 48);
+	else if (nm < 100)
+	    _putchar((nm / 10) + 48), _putchar((nm % 10) + 48);
 	else
 	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				num = i * j;
-
-				if (num < 10)
-				{
-					_putchar(num + 48);
-				}
-				else if (num < 100)
-				{
-					_putchar((num / 10) + 48);
-					_putchar((num % 10) + 48);
-				}
-				else
-				{
-					_putchar((num / 100) + 48);
-					_putchar(((num / 10) % 10) + 48);
-					_putchar((num % 10) + 48);
-				}
-				if (j != n)
-				{
-					_putchar(44);
-					_putchar(32);
-				}
-				if ((i * (j + 1)) < 10)
-				{
-					_putchar(32);
-					_putchar(32);
-				}
-				else if ((i * (j + 1)) < 100)
-				{
-					_putchar(32);
-				}
-			}
-			_putchar(10);
-		}
+	    _putchar((nm / 100) + 48), _putchar(((nm / 10) % 10) + 48);
+	    _putchar((nm % 10) + 48);
 	}
+	if (j != n)
+	    _putchar(44), _putchar(32);
+	if ((i * (j + 1)) < 10)
+	    _putchar(32), _putchar(32);
+	else if ((i * (j + 1)) < 100)
+	    _putchar(32);
+	}
+	_putchar(10);
+    }
+}
 }
