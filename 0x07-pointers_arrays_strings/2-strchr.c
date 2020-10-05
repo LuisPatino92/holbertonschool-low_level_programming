eeab94503ac05239a7d41aca250440237c0c8e9e
LuisPatino92 <2450@holberton.com>
2020-10-05 19:53:54 +0000

    File Creation

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Look for a byte through a string and returns a pointer to
+ *
+ * @s: Direction of the string
+ * @c: The char to look for
+ *
+ * Return: A pointer to the first poin coincidence, null if no coincidence
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+
+	return (0);
+}
