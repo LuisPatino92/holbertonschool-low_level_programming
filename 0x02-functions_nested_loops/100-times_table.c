7a1c3526a605e8a411545986999ebb27e6e4997c
LuisPatino92 <2450@holberton.com>
2020-10-09 00:41:43 +0000

    Correction of spaces

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,33 +9,51 @@
 
 void print_times_table(int n)
 {
-int i, j, nm;
-if (n < 0 || n > 15)
-    _putchar(10);
-else
-{
-    for (i = 0; i <= n; i++)
-    {
-	for (j = 0; j <= n; j++)
+	int i, j, nm;
+
+	if (n < 0 || n > 15)
 	{
-	nm = i * j;
-	if (nm < 10)
-	    _putchar(nm + 48);
-	else if (nm < 100)
-	    _putchar((nm / 10) + 48), _putchar((nm % 10) + 48);
+		_putchar(10);
+	}
 	else
 	{
-	    _putchar((nm / 100) + 48), _putchar(((nm / 10) % 10) + 48);
-	    _putchar((nm % 10) + 48);
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				nm = i * j;
+				if (nm < 10)
+				{
+					_putchar(nm + 48);
+				}
+				else if (nm < 100)
+				{
+					_putchar((nm / 10) + 48);
+					_putchar((nm % 10) + 48);
+				}
+				else
+				{
+					_putchar((nm / 100) + 48);
+					_putchar(((nm / 10) % 10) + 48);
+					_putchar((nm % 10) + 48);
+				}
+
+				if (j != n)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
+				if ((i * (j + 1)) < 10 && (j != n))
+				{
+					_putchar(32);
+					_putchar(32);
+				}
+				else if ((i * (j + 1)) < 100 && (j != n))
+				{
+					_putchar(32);
+				}
+			}
+			_putchar(10);
+		}
 	}
-	if (j != n)
-	    _putchar(44), _putchar(32);
-	if ((i * (j + 1)) < 10)
-	    _putchar(32), _putchar(32);
-	else if ((i * (j + 1)) < 100)
-	    _putchar(32);
-	}
-	_putchar(10);
-    }
-}
 }
