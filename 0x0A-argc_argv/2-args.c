b5534fb197f76ec0c3f29ab06f2ae57d1132dd04
LuisPatino92 <2450@holberton.com>
2020-10-10 23:31:59 +0000

    File creation

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the executable.
+ *
+ * @argc: counter of the arguments.
+ * @argv: array with the inputs.
+ *
+ * Return: Always 0 success
+ */
+
+int main(int __attribute__((unused)) argc, char **argv)
+{
+	for (;*argv; argv++)
+		printf("%s\n", *argv);
+	return (0);
+}
