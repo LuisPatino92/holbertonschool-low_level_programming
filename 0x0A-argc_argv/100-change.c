78fa03d23a8cfb630b45d3e1fdb0de62d2a6c6e6
LuisPatino92 <2450@holberton.com>
2020-10-11 18:03:13 +0000

    File creation

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - calculate amount of coins used as change
+ *
+ * @argc: - Entry parameter, changed after with n
+ * @argv: Array of arguments
+ *
+ * Return: 0 success, 1 otherwise
+ */
+
+int main(int argc, char **argv)
+{
+	int n, i, coins, coin[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argv[1][0] == '-')
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		n = atoi(argv[1]);
+
+		for (i = 0; i < 5; i++)
+		{
+			while (n - coin[i] >= 0)
+			{
+				n = n - coin[i];
+				coins++;
+			}
+		}
+
+		printf("%d\n", coins);
+		return (0);
+	}
+}
