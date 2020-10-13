9f9bebd879ad21d8c9e9cd2d10c4675cf7e1b48f
LuisPatino92 <2450@holberton.com>
2020-10-13 19:16:08 +0000

    Attempt

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,13 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *P, *S1, *S2;
 
+	if (s1 == NULL && s2 == NULL)
+	{
+		P = malloc(sizeof(char));
+		*P = '\0';
+		return (P);
+	}
+
 	if (s1 == NULL)
 	{
 		S1 = malloc(sizeof(char));
@@ -24,6 +31,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		S1 = copy(s1);
 	}
+
 	if (s2 == NULL)
 	{
 		S2 = malloc(sizeof(char));
@@ -33,13 +41,20 @@ char *str_concat(char *s1, char *s2)
 	{
 		S2 = copy(s2);
 	}
+
 	if (malloc(length(S1)) == NULL || malloc(length(S2)) == NULL)
 		return (NULL);
+
 	P = malloc((length(S1) * sizeof(char)) + (length(S2) * sizeof(char)));
+
 	for (i = 0; i <= length(S1); i++)
 		*(P + i) = *(S1 + i);
+
 	for (j = 0; j <= length(S2); j++)
 		*(P + i + j - 1) = *(S2 + j);
+
+	free(S1);
+	free(S2);
 
 	return (P);
 }
