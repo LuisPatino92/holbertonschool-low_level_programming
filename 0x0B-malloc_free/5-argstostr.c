7dba6199763623da6f5da6e125d971aad06c8f5f
LuisPatino92 <2450@holberton.com>
2020-10-14 02:22:00 +0000

    File Creation

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * argstostr - concatenates args in one string sparated by \n
+ *
+ * @ac: Number of arguments
+ * @av: double pointer with all the arguments
+ *
+ * Return: The pointer to the concatenated string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *concatenated;
+	int i, j, k = 0, sum = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 1; i < ac; i++)
+		sum += length(*(av + i));
+
+	concatenated = malloc((sum + 1) * (sizeof(char)));
+
+	if (concatenated == NULL)
+		return (NULL);
+
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++)
+		{
+			*(concatenated + k) = *(*(av + i) + j);
+			k++;
+		}
+		*(concatenated + k) = '\n';
+		k++;
+	}
+	*(concatenated + k) = '\0';
+
+	return (concatenated);
+}
+
+/**
+ * length - Computes the length of a string recursively
+ *
+ * @str: The string whose length you want to compute
+ *
+ * Return: The length of the string
+ */
+
+int length(char *str)
+{
+	if (*(str) == '\0')
+		return (1);
+	else
+		return (1 + length(str + 1));
+
+}
