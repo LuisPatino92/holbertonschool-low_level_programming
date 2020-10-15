ff013a59392adf9e84e5e2afd24641d1ddcf54e8
LuisPatino92 <2450@holberton.com>
2020-10-15 19:06:29 +0000

    Hope

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,56 +18,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *P;
 	unsigned int s2_contribution, i, j;
 
-	if (length(s2) > n)
+	if (length(s2) + 1 > n)
 		s2_contribution = n;
 	else
 		s2_contribution = length(s2) + 1;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		P = malloc(1);
-		*(P) = '\0';
-		return (P);
-	}
-	else if (s1 == NULL)
-	{
-		P = (char *)malloc((s2_contribution + 1) * sizeof(char));
-		if (P == NULL)
-		{
-			free(P);
-			return (NULL);
-		}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-		for (i = 0; i < s2_contribution; i++)
-			*(P + i) = *(s2 + i);
-		return (P);
-	}
-	else if (s2 == NULL)
+	P = (char *)malloc((length(s1) + s2_contribution) * sizeof(char));
+
+	if (P == NULL)
 	{
-		P = (char *)malloc((length(s1) + 1) * sizeof(char));
-		if (P == NULL)
-		{
-			free(P);
-			return (NULL);
-		}
-		for (i = 0; i <= length(s1); i++)
-			*(P + i) = *(s1 + i);
-		return (P);
+		free(P);
+		return (NULL);
 	}
-	else
-	{
-		P = (char *)malloc((length(s1) + s2_contribution + 1) * sizeof(char));
-		if (P == NULL)
-		{
-			free(P);
-			return (NULL);
-		}
-		for (i = 0; i <= length(s1); i++)
-			*(P + i) = *(s1 + i);
-		for (j = 0; j < s2_contribution; j++)
-			*(P + i + j - 1) = *(s2 + j);
-		return (P);
-	}
+
+	for (i = 0; i < length(s1); i++)
+		*(P + i) = *(s1 + i);
+
+	for (j = 0; j <= s2_contribution; j++)
+		*(P + i + j - 1) = *(s2 + j);
+
+	return (P);
 
 }
 
