29c2b8171f2f6fe4716567476b14a5f886c2835d
LuisPatino92 <2450@holberton.com>
2020-10-17 15:29:32 +0000

    More word separator included

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,30 +10,34 @@
 char *cap_string(char *toUP)
 {
 
-	int i, j;
-	char spaces[] = {32, 9, , 10, 44, 59, 46, 33, 63, 34, 40, 41, };
+	int i, j, k;
+	char spaces[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (i = 0; *(toUP + i); i++)
+
+	for (k = 0; k <= 12; k++)
 	{
-		if (*(toUP + i) == 32)
+		for (i = 0; *(toUP + i); i++)
 		{
-			for (j = 97; j <= 122; j++)
+			if (*(toUP + i) == spaces[k])
 			{
-				if (toUP[i + 1] == j)
-					toUP[i + 1] = j - 32;
-			}
-		}
-
-		if (i == 0)
-		{
-			for (j = 97; j <= 122; j++)
-			{
-				if (toUP[i] == j)
-					toUP[i] = j - 32;
+				for (j = 97; j <= 122; j++)
+				{
+					if (toUP[i + 1] == j)
+						toUP[i + 1] = j - 32;
+				}
 			}
 
-		}
+			if (i == 0)
+			{
+				for (j = 97; j <= 122; j++)
+				{
+					if (toUP[i] == j)
+						toUP[i] = j - 32;
+				}
 
+			}
+
+		}
 	}
 
 	return (toUP);
