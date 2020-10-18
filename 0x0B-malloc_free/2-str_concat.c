6e44c293271baed60f6d4e6a2465d65439e2d1ed
LuisPatino92 <2450@holberton.com>
2020-10-18 03:49:27 +0000

    Modification

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,44 +13,25 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, j;
-	char *P, *S1, *S2;
+	char *P;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		P = malloc(sizeof(char));
-		*P = '\0';
-		return (P);
-	}
 	if (s1 == NULL)
-	{
-		S1 = malloc(sizeof(char));
-		*S1 = '\0';
-	}
-	else
-		S1 = copy(s1);
+		s1 = "";
+
 	if (s2 == NULL)
-	{
-		S2 = malloc(sizeof(char));
-		*S2 = '\0';
-	}
-	else
-		S2 = copy(s2);
+		s2 = "";
 
-	/*	if (malloc(length(S1)) == NULL || malloc(length(S2)) == NULL)
-		return (NULL);*/
-
-	P = malloc((length(S1) + length(S2) + 1) * sizeof(char));
+	P = malloc((length(s1) + length(s2) + 1) * sizeof(char));
 	if (P == NULL)
 		return (NULL);
 
-	for (i = 0; i <= length(S1); i++)
-		*(P + i) = *(S1 + i);
+	for (i = 0; i < length(s1); i++)
+		*(P + i) = *(s1 + i);
 
-	for (j = 0; j <= length(S2); j++)
-		*(P + i + j - 1) = *(S2 + j);
+	for (j = 0; j < length(s2); j++)
+		*(P + i + j) = *(s2 + j);
 
-	free(S1);
-	free(S2);
+	*(P + i + j) = '\0';
 
 	return (P);
 }
@@ -66,31 +47,10 @@ char *str_concat(char *s1, char *s2)
 
 int length(char *str)
 {
-	if (*(str) == '\0')
-		return (0);
-	else
-		return (1 + length(str + 1));
-
-}
-
-/**
- * copy - copies a string to other modificable
- *
- * @string: The string to be copied
- *
- * Return: The pointer to copy
- */
-
-char *copy(char *string)
-{
-	char *P;
 	int i;
 
-	P = malloc((length(string) * sizeof(char)) + 1);
+	for (i = 0; *(str + i); i++)
+		;
 
-	for (i = 0; i <= length(string); i++)
-		*(P + i) = *(string + i);
-
-	return (P);
-
+	return (i);
 }
