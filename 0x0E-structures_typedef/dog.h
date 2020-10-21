d0ea9d7648d4ebbfea1e98b44e95dd32944467a9
Lucho <2450@holbertonschool.com>
2020-10-21 05:10:40 +0000

    Prototypes at the end

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * struct dog - A dog class structure
  * @name: The dog's name
@@ -20,5 +15,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
