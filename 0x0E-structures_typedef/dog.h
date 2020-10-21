2a74c22926e86cb47b1fb5047ec90f0622566736
Lucho <2450@holbertonschool.com>
2020-10-21 05:05:32 +0000

    Print added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
+struct dog;
+void print_dog(struct dog *d);
 
 /**
  * struct dog - A dog class structure
