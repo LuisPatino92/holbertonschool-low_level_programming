30d9a1e7ba60c0d4a1a91338864f5cbf8b42b997
Lucho <2450@holbertonschool.com>
2020-10-21 05:08:29 +0000

    Prototype order changed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 struct dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 /**
