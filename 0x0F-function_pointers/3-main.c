38eeb3193e561ac3d20aef73bff518af98f6b245
Lucho <2450@holbertonschool.com>
2020-10-22 05:06:07 +0000

    Betty style fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,8 +23,8 @@ int main(int argc, char **argv)
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && (argv[3][0] == '0'))
 	{
-			printf("Error\n");
-			exit(100);
+		printf("Error\n");
+		exit(100);
 	}
 
 	for (i = 0; i < 5; i++)
