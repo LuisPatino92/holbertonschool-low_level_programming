699b3d5e47e4c1cc2cc091866898efb5bdaca825
Lucho <2450@holbertonschool.com>
2020-10-22 05:15:36 +0000

    Requierment fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 
 /**
@@ -27,6 +28,8 @@ int (*get_op_func(char *s))(int, int)
 			return (ops[i].f);
 		i++;
 	}
+	printf("Error\n");
+	exit(99);
 	return (NULL);
 
 }
