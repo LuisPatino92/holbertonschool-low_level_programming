df586c85e1ffb090849823f72b9ef952d9ab2aca
LuisPatino92 <2450@holbertonschool.com>
2020-10-30 22:57:31 +0000

    File Creation

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void turtle(void) __attribute__ ((constructor));
+
+/**
+ * turtle - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void turtle(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
