9684402642a43ec8f3ff340edcbafef5036e2dd2
LuisPatino92 <2450@holbertonschool.com>
2020-10-30 16:14:44 +0000

    File creation

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a linked list
+ *
+ * @h: The list to be printed
+ *
+ * Returns: The number of nodes printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+
+		h = h->next;
+	}
+
+	return (i);
+}
