e44f2ae1f7ca4b1930c9214eace2cdaa5066f1a2
LuisPatino92 <2450@holbertonschool.com>
2020-11-04 17:34:35 +0000

    A police at the entry

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	while (*head != NULL)
+	aux = *head;
+
+	if (*head == NULL)
+		return;
+
+	while (aux != NULL)
 	{
-		aux = (*head)->next;
-		free(*head);
-		*head = aux;
+		*head = aux->next;
+		free(aux);
+		aux = *head;
 	}
-	*head = NULL;
+
 }
