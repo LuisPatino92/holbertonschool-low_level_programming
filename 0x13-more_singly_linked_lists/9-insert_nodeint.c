a8c0d61c8a4d06e96472ea15fb6132d70918d888
LuisPatino92 <2450@holbertonschool.com>
2020-11-04 17:45:52 +0000

    Posibility of idx = 0 handled

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,10 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *current = *head, *new_node = NULL;
 
-	if (head == NULL)
-		return (NULL);
-
-	for (i = 0; current != NULL; i++)
+	for (i = 0; current != NULL || idx == 0; i++)
 	{
 		if (i + 1 == idx || idx == 0)
 		{
