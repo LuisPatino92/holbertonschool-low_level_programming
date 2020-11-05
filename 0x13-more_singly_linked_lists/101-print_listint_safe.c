1f2b1ddd647bf5dab1f536a05ea6700a10693ebd
LuisPatino92 <2450@holbertonschool.com>
2020-11-05 03:45:37 +0000

    Improving hash map

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,10 +11,7 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0, j;
-	const listint_t *aux, *hash_map[40];
-
-	if (head == NULL)
-		exit(98);
+	const listint_t *aux, *hash_map[100];
 
 	aux = head;
 
