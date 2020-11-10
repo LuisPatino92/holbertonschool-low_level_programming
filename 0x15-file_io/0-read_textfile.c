578b0b4be56dbd9d80bee03bb7c63ae27968e9eb
LuisPatino92 <2450@holbertonschool.com>
2020-11-10 14:35:11 +0000

    Filename efectively used

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,20 +18,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open("Requiescat", O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	buffer = malloc(letters * sizeof(char));
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
+
 	chars_readed = read(fd, buffer, letters);
 	if (chars_readed == -1)
 	{
-		close(fd);
 		free(buffer);
 		return (0);
 	}
