6a8f609808223c92a8fa11a048442e751b905c44
LuisPatino92 <2450@holbertonschool.com>
2020-11-10 15:14:29 +0000

    Create file, File creation

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+
+int length(char *str);
+
+/**
+ * create_file - Creates a file with desired text inside
+ *
+ * @filename: The name for the created filename
+ * @text_content: The text to write inside the file
+ *
+ * Return: 1 on success, -1 otherwise
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	written = write(fd, text_content, length(text_content));
+	if (written == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
+
+/**
+ * length - Measure the length of a string
+ *
+ * @str: The string whose length is needed
+ *
+ * Return: The length of the string (number of elements)
+ */
+
+int length(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i); i++)
+		;
+
+	return (i);
+}
