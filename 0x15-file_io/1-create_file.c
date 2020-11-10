4e836f8d37d44c7a0842c04028f4e6cfec78ef8e
LuisPatino92 <2450@holbertonschool.com>
2020-11-10 15:19:55 +0000

    NULL text_content handled 2.0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		written = write(fd, text_content, length(text_content));
 	else
-		written = write(fd, "", length(text_content));
+		written = write(fd, "", 0);
 
 	if (written == -1)
 	{
