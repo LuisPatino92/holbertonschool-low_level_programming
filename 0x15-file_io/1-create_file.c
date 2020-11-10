a39d2d9a668c58e5525a6764cb60dd3145d5ff26
LuisPatino92 <2450@holbertonschool.com>
2020-11-10 15:39:19 +0000

    Betty Style Correction 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,8 +23,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-		written = write(fd, text_content, length(text_content));
+	written = write(fd, text_content, length(text_content));
 
 	if (written == -1)
 	{
