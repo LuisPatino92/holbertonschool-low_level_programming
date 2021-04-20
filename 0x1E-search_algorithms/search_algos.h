5d67be3e27e2e1e62c860b43389d78d416cb89ed
LuisPatino92 <2450@holbertonschool.com>
2021-04-20 13:02:55 +0000

    Prototypes of linear and binary added

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCHING
+#define SEARCHING
+
+#include <stdio.h>
+
+/*Linear Search*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /* SEARCHING */
