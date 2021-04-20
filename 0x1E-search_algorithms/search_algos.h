6220d1d802c3c7718fd7ef8df747063adc83d673
LuisPatino92 <2450@holbertonschool.com>
2021-04-20 17:05:32 +0000

    Task 1 upload

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,7 +5,9 @@
 
 /*Linear Search*/
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
 
+/*Binary search*/
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int size);
 
 #endif /* SEARCHING */
