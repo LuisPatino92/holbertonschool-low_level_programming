3160b066d67ef42f20795736d7ad3c619f41312b
LuisPatino92 <2450@holbertonschool.com>
2021-04-21 02:34:23 +0000

    Betty II

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,17 +12,17 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-unsigned int i = 0;
+	unsigned int i = 0;
 
-if (array == NULL)
-return (-1);
+	if (array == NULL)
+		return (-1);
 
-for (i = 0; i < size; i++)
-{
-printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
-if (value == *(array + i))
-return (i);
-}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)i, *(array + i));
+		if (value == *(array + i))
+			return (i);
+	}
 
-return (-1);
+	return (-1);
 }
