3e13e443a5155eab2fa7649a06667851bfa4c7c2
LuisPatino92 <2450@holbertonschool.com>
2021-04-21 12:51:26 +0000

    Jump Search prototypes added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,7 @@
 #define SEARCHING
 
 #include <stdio.h>
+#include <math.h>
 
 /*Linear Search*/
 int linear_search(int *array, size_t size, int value);
@@ -9,5 +10,8 @@ int linear_search(int *array, size_t size, int value);
 /*Binary search*/
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int size);
+
+/*Jump Search*/
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCHING */
